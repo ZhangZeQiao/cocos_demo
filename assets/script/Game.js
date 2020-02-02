5457eb96141608e03fb43cb41337bb2baf52f646
zhangzeqiao <zhangzeqiao@xmiles.cn>
2020-02-02 16:25:47 +0800

    Label的引入及得分逻辑

--- assets/script/Game.js
+++ assets/script/Game.js
@@ -30,6 +30,11 @@ cc.Class({
         player: {
             default: null,
             type: cc.Node
+        },
+        // score label 的引用
+        scoreDisplay: {
+            default: null,
+            type: cc.Label
         }
     },
 
@@ -38,8 +43,13 @@ cc.Class({
     onLoad: function () {
         // 获取地平面的 y 轴坐标
         this.groundY = this.ground.y + this.ground.height / 2;
+        // 初始化计时器
+        this.timer = 0;
+        this.starDuration = 0;
         // 生成一个新的星星
         this.spawnNewStar();
+        // 初始化计分
+        this.score = 0;
     },
 
     spawnNewStar: function () {
@@ -52,6 +62,10 @@ cc.Class({
 
         // 在星星组件上暂存 Game 对象的引用
         newStar.getComponent('Star').game = this;
+
+        // 重置计时器，根据消失时间范围随机取一个值
+        this.starDuration = this.minStarDuration + Math.random() * (this.maxStarDuration - this.minStarDuration);
+        this.timer = 0;
     },
 
     getNewStarPosition: function () {
@@ -69,5 +83,31 @@ cc.Class({
 
     },
 
-    // update (dt) {},
+    update: function (dt) {
+        // 每帧更新计时器，超过限度还没有生成新的星星
+        // 就会调用游戏失败逻辑
+        if (this.timer > this.starDuration) {
+            this.gameOver();
+            return;
+        }
+        this.timer += dt;
+    },
+
+    gainScore: function () {
+        this.score += 1;
+        // 更新 scoreDisplay Label 的文字
+        this.scoreDisplay.string = 'Score: ' + this.score;
+    },
+
+    gameOver: function () {
+        this.player.stopAllActions(); //停止 player 节点的跳跃动作
+        cc.director.loadScene('game');
+
+        /* 
+        这里需要初学者了解的是，cc.director 是一个管理你的游戏逻辑流程的单例对象。
+        由于 cc.director 是一个单例，你不需要调用任何构造函数或创建函数，
+        使用它的标准方法是通过调用 cc.director.methodName()，例如这里的 cc.director.loadScene('game') 就是重新加载游戏场景 game，也就是游戏重新开始。
+        而节点下的 stopAllActions 方法就显而易见了，这个方法会让节点上的所有 Action 都失效。
+        */
+    }
 });
